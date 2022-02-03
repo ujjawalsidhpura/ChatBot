313c1c3c8bfc2b501d939d562789d92224d8e263
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-02-03 12:35:03 -0500

    recuring pickle model

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 import numpy
 import nltk
 import tflearn
+import pickle
 
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
@@ -11,56 +12,63 @@ stemmer = LancasterStemmer()
 with open('commands.json') as file:
     data = json.load(file)
 
-words = []
-labels = []
-docs_a = []
-docs_b = []
+try:
+    with open('data.pickle', 'rb') as f:
+        words, training, labels, output = pickle.load(f)
+except:
 
-# Loop through commands to filter out sentences and then use nltk to convert those sentences into array of words
-for command in data['commands']:
-    for pattern in command['patterns']:
-        words_list = nltk.word_tokenize(pattern)
-        words.extend(words_list)  # Add this list to words array
-        docs_a.append(pattern)  # Also add pattern to docs
-        # Corresponding tag to a pattern in docs_a
-        docs_b.append(command['tag'])
+    words = []
+    labels = []
+    docs_a = []
+    docs_b = []
 
-    if command['tag'] not in labels:
-        labels.append(command['tag'])
+    # Loop through commands to filter out sentences and then use nltk to convert those sentences into array of words
+    for command in data['commands']:
+        for pattern in command['patterns']:
+            words_list = nltk.word_tokenize(pattern)
+            words.extend(words_list)  # Add this list to words array
+            docs_a.append(pattern)  # Also add pattern to docs
+            # Corresponding tag to a pattern in docs_a
+            docs_b.append(command['tag'])
 
+        if command['tag'] not in labels:
+            labels.append(command['tag'])
 
-# Stem it, also filter out '?' from data model
-words = [stemmer.stem(w.lower()) for w in words if w != '?']
-# Remove duplicates from stemmed set, convert back to list and sort it
-words = sorted(list(set(words)))
-labels = sorted(labels)
+    # Stem it, also filter out '?' from data model
+    words = [stemmer.stem(w.lower()) for w in words if w != '?']
+    # Remove duplicates from stemmed set, convert back to list and sort it
+    words = sorted(list(set(words)))
+    labels = sorted(labels)
 
-# Creating lists to train the function
+    # Creating lists to train the function
 
-training = []
-output = []
+    training = []
+    output = []
 
-out_empty = [0 for _ in range(len(labels))]
+    out_empty = [0 for _ in range(len(labels))]
 
-for a, doc in enumerate(docs_a):
-    basket = []
+    for a, doc in enumerate(docs_a):
+        basket = []
 
-    words_list = [stemmer.stem(w) for w in doc]
+        words_list = [stemmer.stem(w) for w in doc]
 
-    for w in words:
-        if w in words_list:
-            basket.append(1)
-        else:
-            basket.append(0)
+        for w in words:
+            if w in words_list:
+                basket.append(1)
+            else:
+                basket.append(0)
 
-    output_row = out_empty[:]
-    output_row[labels.index(docs_b[a])] = 1
+        output_row = out_empty[:]
+        output_row[labels.index(docs_b[a])] = 1
 
-    training.append(basket)
-    output.append(output_row)
+        training.append(basket)
+        output.append(output_row)
 
-training = numpy.array(training)
-output = numpy.array(output)
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open('data.pickle', 'wb') as f:
+        pickle.dump((words, training, labels, output), f)
 
 
 tensorflow.reset_default_graph()
