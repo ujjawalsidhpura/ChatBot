ba5f05d60f153067c6080b61c13aeb3ba53941b4
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-02-03 10:38:52 -0500

    create hot coded training list

--- main.py
+++ main.py
@@ -13,14 +13,47 @@ with open('commands.json') as file:
 
 words = []
 labels = []
-docs = []
+docs_a = []
+docs_b = []
 
 # Loop through commands to filter out sentences and then use nltk to convert those sentences into array of words
 for command in data['commands']:
     for pattern in command['patterns']:
         words_list = nltk.word_tokenize(pattern)
         words.extend(words_list)  # Add this list to words array
-        docs.append(pattern)  # Also add pattern to docs
+        docs_a.append(pattern)  # Also add pattern to docs
+        # Corresponding tag to a pattern in docs_a
+        docs_b.append(command['tag'])
 
     if command['tag'] not in labels:
         labels.append(command['tag'])
+
+
+words = [stemmer.stem(w.lower()) for w in words]
+# Remove duplicates from stemmed set, convert back to list and sort it
+words = sorted(list(set(words)))
+labels = sorted(labels)
+
+# Creating lists to train the function
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for a, doc in enumerate(docs_a):
+    basket = []
+
+    words_list = [stemmer.stem(w) for w in doc]
+
+    for w in words:
+        if w in words_list:
+            basket.append(1)
+        else:
+            basket.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_b[a])] = 1
+
+    training.append(basket)
+    output.append(output_row)
